479897e9ccfef419e823e5e33ab5c42424c92e7f
Dieter Plaetinck <dieter@plaetinck.be>
2024-02-21 21:02:54 +0200

    cleanup + allow setting a custom honeypot form value

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"sort"
 	"strings"
@@ -15,19 +16,21 @@ import (
 
 var path string
 var addr string
+var special string
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "%s <comments-path> <addr>\n", os.Args[0])
+	if len(os.Args) != 3 && len(os.Args) != 4 {
+		fmt.Fprintf(os.Stderr, "%s <comments-path> <addr> [special-value]\n", os.Args[0])
 		os.Exit(2)
 	}
-	path = os.Args[1]
-	if strings.HasSuffix(path, "/") {
-		path = path[:len(path)-1]
-	}
+	path = strings.TrimSuffix(os.Args[1], "/")
+
 	addr = os.Args[2]
 	fmt.Println("looking for comments in", path)
 	fmt.Println("will listen for http traffic on", addr)
+	if len(os.Args) == 4 {
+		special = os.Args[3]
+	}
 
 	http.Handle("/", cors.Default().Handler(
 		http.HandlerFunc(
@@ -45,38 +48,35 @@ func main() {
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	err := r.ParseMultipartForm(1024 * 10)
 	if err != nil {
 		fmt.Println(r.URL.Path, "POST", "unparseable form", err)
 		http.Error(w, "Couldn't parse form: "+err.Error(), http.StatusBadRequest)
 		return
 	}
-	// honey pot
-	if r.Form.Get("company") != "" {
-		fmt.Println(r.URL.Path, "POST", "company not empty:", r.Form.Get("company"))
-		http.Error(w, "ok", http.StatusTeapot)
+
+	if special != "" && r.Form.Get("special") != special {
+		fmt.Println(r.URL.Path, "POST", "bad 'special' value", r.Form.Get("special"))
+		http.Error(w, "incorrect value provided", http.StatusForbidden)
 		return
 	}
 
-	c := Comment{
-		Parent:    r.Form.Get("post"),
-		Ts:        time.Now(),
-		Message:   r.Form.Get("message"),
-		Ipaddress: "", // TODO
-		Author:    r.Form.Get("name"),
-		Email:     r.Form.Get("email"),
-		Link:      r.Form.Get("url"),
+	c, err := CommentFromForm(r.Form)
+	if err != nil {
+		fmt.Println(r.URL.Path, "POST", "invalid submission", err)
+		http.Error(w, "Invalid submission: "+err.Error(), http.StatusBadRequest)
+		return
 	}
-	c.Hash = fmt.Sprintf("%x", md5.Sum([]byte(c.Email)))
 
 	err = c.Save(path)
 	if err != nil {
-		fmt.Println(r.URL.Path, "POST", "Couldn't save comment: ", err)
+		fmt.Println(r.URL.Path, "POST", "Error: Couldn't save comment: ", err)
 		http.Error(w, "Couldn't save comment: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	bytes, e := json.Marshal(c)
 	if e != nil {
+		fmt.Println(r.URL.Path, "POST", "Error marshalling JSON: ", err)
 		http.Error(w, fmt.Sprintf("Error marshalling JSON:'%s'", e), http.StatusInternalServerError)
 		return
 	}
@@ -84,16 +84,44 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
+func CommentFromForm(form url.Values) (Comment, error) {
+	if form.Get("message") == "" {
+		return Comment{}, fmt.Errorf("message is required")
+	}
+	if form.Get("name") == "" {
+		return Comment{}, fmt.Errorf("name is required")
+	}
+	if form.Get("email") == "" {
+		return Comment{}, fmt.Errorf("email is required")
+	}
+	if form.Get("post") == "" {
+		return Comment{}, fmt.Errorf("post is required")
+	}
+	return Comment{
+		Parent:    form.Get("post"),
+		Ts:        time.Now(),
+		Message:   form.Get("message"),
+		Ipaddress: "", // TODO
+		Author:    form.Get("name"),
+		Email:     form.Get("email"),
+		Link:      form.Get("url"),
+		Hash:      fmt.Sprintf("%x", md5.Sum([]byte(form.Get("email")))),
+	}, nil
+
+}
+
 func handleGet(w http.ResponseWriter, r *http.Request) {
 	// strip out all /prependend/path/separators/if/any so that it works under arbitrary paths, for proxying etc
 	cutoff := strings.LastIndex(r.URL.Path, "/")
 	slug := r.URL.Path[cutoff+1:]
 	if slug == "" {
-		http.Error(w, "specify a slug, you dufus", http.StatusMethodNotAllowed)
+		fmt.Println(r.URL.Path, "GET", "No slug specified")
+		http.Error(w, "specify a slug, you dufus", http.StatusBadRequest)
 		return
 	}
 	comments, err := FindComments(path, slug)
 	if err != nil {
+		fmt.Println(r.URL.Path, "GET", "Error: FindComments() failed: "+err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -101,6 +129,7 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("> %s -> serving %d comments\n", slug, len(comments))
 	bytes, e := json.Marshal(comments)
 	if e != nil {
+		fmt.Println(r.URL.Path, "GET", "Error marshalling JSON: "+err.Error())
 		http.Error(w, fmt.Sprintf("Error marshalling JSON:'%s'", e), http.StatusInternalServerError)
 		return
 	}
